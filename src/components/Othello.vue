a0b90b6e43a0400d57452781f6ea239ef0330501
metz <daryljmetzler@gmail.com>
2025-11-25 19:36:53 -0800

    Switch to Vue

---
+++ src/components/Othello.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="game-frame">
+    <div class="status-bar">
+      <p>Foo</p>
+      <p>Foo</p>
+      <p>Foo</p>
+    </div>
+    <div class="game-board">
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import StatusBar from './StatusBar.vue'
+  console.log('foo');
+</script>
