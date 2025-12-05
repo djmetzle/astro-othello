eccc06883b0a177d31b6d43e037ac9bdc9d689fa
metz <daryljmetzler@gmail.com>
2025-12-04 19:14:45 -0800

    Display counts in statusbar

--- src/components/StatusBar.vue
+++ src/components/StatusBar.vue
@@ -4,10 +4,12 @@
           <div class="token white" v-if="turn == Token.White"></div>
           <div class="token" v-if="turn == Token.Black"></div>
     </div>
+    <p>{{counts[0]}}</p>
     <p v-if="!winner">Othello</p>
     <p v-if="winner === 'tie'">TIED!</p>
     <p class="winner" v-if="winner === Token.White">White Wins!</p>
     <p v-if="winner === Token.Black">Black Wins!</p>
+    <p>{{counts[1]}}</p>
     <div class="token-status">
           <div class="token" v-if="turn == Token.White"></div>
           <div class="token black" v-if="turn == Token.Black"></div>
@@ -22,4 +24,5 @@
   const game = inject('game')
   const turn = computed(() => game.value.current_turn())
   const winner = computed(() => game.value.winner())
+  const counts = computed(() => game.value.board.counts());
 </script>
